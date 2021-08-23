2361a7600d1f50ff3adac208d3148ebf1bff9cff
Angel D. Munoz <scythes.lol@gmail.com>
2021-08-22 19:00:02 -0500

    add some of the missing directives

--- src/Lit.fs
+++ src/Lit.fs
@@ -2,6 +2,7 @@ module Lit
 
 open Fable.Core.JsInterop
 open Browser.Types
+open Fable.Core
 
 type TemplateResult =
     interface end
@@ -11,6 +12,13 @@ module Bindings =
     let svg: Template.JsTag<TemplateResult> = importMember "lit-html"
     let render (t: TemplateResult) (el: HTMLElement): unit = importMember "lit-html"
     let styleMap (styles: obj): obj = importMember "lit-html/directives/style-map"
+
+    let classMap (classes: obj): obj = importMember "lit-html/directives/class-map"
+    let private _until: obj = import "until" "lit-html/directives/until"
+
+    [<Emit("Bindings__until(...$0)")>]
+    let until (values: obj[]): TemplateResult = jsNative
+
 
 let html: Template.Tag<_> = Template.transform Bindings.html
 
@@ -26,3 +34,16 @@ let styles (styles: Feliz.CssStyle seq) =
     |> Seq.iter (fun (Feliz.CssStyle(key, value)) ->
         map?(key) <- value)
     Bindings.styleMap map
+    
+let classes (classes: (string * bool) seq) = 
+    Bindings.classMap (keyValueList CaseRules.LowerFirst classes)
+
+let repeat<'T> (items: 'T seq) (template: 'T -> int -> TemplateResult) = importMember "lit-html/directives/repeat"
+
+let cache (template: TemplateResult): TemplateResult = importMember "lit-html/directives/cache"
+
+let guard (deps: obj[]) (render: unit -> TemplateResult): TemplateResult = importMember "lit-html/directives/guard"
+
+let until (values: obj[]) (placeholder: TemplateResult) =
+    let result = [| yield! values; placeholder |]
+    Bindings.until result
