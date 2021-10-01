6d4eda2ca86b71009ed116b012691942cae14a4b
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-10-01 14:09:11 +0900

    Updates

--- resources/nacara-standard-layouts/scripts/menu.js
+++ resources/nacara-standard-layouts/scripts/menu.js
@@ -1,64 +1,3 @@
-const setupMenuNavigation = () => {
-    /*
-     * Initialize the menu state
-     */
-
-    // Collapse menu-group which doesn't concerns the current page
-    document
-        .querySelectorAll(`.menu .menu-group[data-collapsible=true][data-default-state=collapsed]`)
-        .forEach(function (menuGroup) {
-            var parentChildren = Array.from(menuGroup.parentElement.children);
-
-            var subItems =
-                parentChildren.find(function (child) {
-                    return child.nodeName === "UL";
-                });
-
-            // A menu-group is expanded when one of it's element is tagged as `is-active`
-            // This takes care of nested menus
-            const isActiveMenu =
-                menuGroup.parentElement.querySelector(".is-active") !== null
-
-            if (isActiveMenu) {
-                menuGroup.classList.add("is-expanded");
-                subItems.style.display = "block";
-            } else {
-                menuGroup.classList.remove("is-expanded");
-                subItems.style.display = "none";
-            }
-        });
-
-    // Register listener to handle menu-group
-    document
-        .querySelectorAll(`.menu .menu-group[data-collapsible=true]`)
-        .forEach(function (menuGroup) {
-            menuGroup.addEventListener("click", function (ev) {
-                // All the menu "calculation" are done relative to the .menu-group element
-                var menuGroup =
-                    ev.target.classList.contains("menu-group")
-                        ? ev.target
-                        : ev.target.closest(".menu-group");
-
-                var parentChildren = Array.from(menuGroup.parentElement.children);
-
-                var subItems =
-                    parentChildren.find(function (child) {
-                        return child.nodeName === "UL";
-                    });
-
-                if (menuGroup.classList.contains("is-expanded")) {
-                    menuGroup.classList.remove("is-expanded");
-                    subItems.style.display = "none";
-                } else {
-                    menuGroup.classList.add("is-expanded");
-                    subItems.style.display = "block";
-                }
-
-            });
-        });
-
-}
-
 const setupNavbarBurger = () => {
 
     /*
@@ -68,25 +7,17 @@ const setupNavbarBurger = () => {
     // Code copied from Bulma documentation
     // https://bulma.io/documentation/components/navbar/#navbar-menu
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
+    const $navbarBurgerDots = document.querySelector(".navbar-burger-dots");
 
-        // Add a click event on each of them
-        $navbarBurgers.forEach(el => {
-            el.addEventListener('click', () => {
+    if ($navbarBurgerDots !== null) {
+        $navbarBurgerDots.addEventListener("click", (ev) => {
+            const $nacaraNavbarMenu = document.querySelector(".nacara-navbar-menu");
 
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-
-            });
+            if ($nacaraNavbarMenu !== null) {
+                $nacaraNavbarMenu.classList.toggle("is-active");
+                $navbarBurgerDots.classList.toggle("is-active");
+            }
         });
     }
 
@@ -155,31 +86,126 @@ const setupCopyCode = () => {
         });
 }
 
+const setupNavbarDropdown = () => {
+    document
+        .querySelectorAll(".navbar-item.has-nacara-dropdown")
+        .forEach(function (navbarItem) {
+            navbarItem.addEventListener("click", (ev) => {
+                // Click is inside the dropdown element, nothing to do
+                if (ev.target.closest(".nacara-dropdown")) {
+                    return;
+                }
+
+                const $activeDropdownItem = document.querySelector(".navbar-item.has-nacara-dropdown.is-active");
+
+                // If we clicked on the active dropdown close it
+                if ($activeDropdownItem) {
+                    const activeItemGuid = $activeDropdownItem.attributes.getNamedItem("data-guid").value;
+                    const clickedItemGuid = navbarItem.attributes.getNamedItem("data-guid").value;
+
+                    // If user clicked on the active dropdown item, close everything
+                    if (activeItemGuid === clickedItemGuid) {
+                        // Close the dropdown
+                        $activeDropdownItem.classList.remove("is-active");
+                        // Remove the overlay
+                        $greyOverlay.classList.remove("is-active");
+                    } else {
+                        // The user clicked on another dropdown item, close the active one
+                        $activeDropdownItem.classList.remove("is-active");
+                        // Show the new dropdown as active
+                        navbarItem.classList.add("is-active");
+                    }
+                } else {
+                    // Show the overlay
+                    $greyOverlay.classList.add("is-active");
+                    // Show which dropdown is active
+                    navbarItem.classList.add("is-active");
+                }
+            });
+        })
+
+    const navbarDropdownTimeoutCache = {};
+
+    // Fullwidth dropdown needs to handle the hover event via JavaScript to add a small delay
+    // before hiding the dropdown when the mouse leave
+    // This is because when displaying a dropdown in fullwidth there is a gap between
+    // the dropdown-link and the dropdown content
+    document.querySelectorAll(".navbar-item.has-nacara-dropdown.is-fullwidth")
+        .forEach(function (navbarItem) {
+            navbarItem.addEventListener("mouseenter", function() {
+                const itemGuid = navbarItem.attributes.getNamedItem("data-guid").value;
+
+                // If there is already a timeout for this dropdown cancel it
+                if (navbarDropdownTimeoutCache[itemGuid]) {
+                    clearTimeout(navbarDropdownTimeoutCache[itemGuid]);
+                    delete navbarDropdownTimeoutCache[itemGuid];
+                }
+
+                // Add the CSS class which force to show the dropdown from the JS
+                navbarItem.classList.add("is-hovered-js");
+
+                // When the mouse leave, wait a small amount of time before removing the CSS class
+                navbarItem.addEventListener("mouseleave", function(ev) {
+                    const timeoutId = setTimeout(() => {
+                        navbarItem.classList.remove("is-hovered-js");
+                        delete navbarDropdownTimeoutCache[itemGuid];
+                    }, 200);
+                    // Store the timeoutID so we can cancel it if the user make some back and forth
+                    // between the link and the dropdown content
+                    navbarDropdownTimeoutCache[itemGuid] = timeoutId;
+                }, { once : true});
+            })
+        })
+
+    const $greyOverlay = document.querySelector(".grey-overlay");
+
+    if ($greyOverlay !== null) {
+        $greyOverlay.addEventListener("click", (ev) => {
+            const $activeDropdownItem = document.querySelector(".navbar-item.has-nacara-dropdown.is-active");
+
+            if ($activeDropdownItem) {
+                $activeDropdownItem.classList.remove("is-active");
+                $greyOverlay.classList.remove("is-active");
+            }
+        });
+    }
+}
+
+const scrollMenuOrTableOfContentIfNeeded = () => {
+
+    // Make the table of content visible
+    const $tableOfContentElement = document.querySelector(".table-of-content");
+    const $activeMenuItemElement = document.querySelector(".menu .is-active");
+
+    if ($tableOfContentElement !== null) {
+        scrollIntoView($tableOfContentElement, {
+            scrollMode: "if-needed",
+            block: 'nearest',
+            inline: 'nearest',
+            boundary: document.querySelector(".menu-container")
+        });
+    } else if ($activeMenuItemElement !== null) {
+        scrollIntoView($activeMenuItemElement, {
+            scrollMode: "if-needed",
+            block: 'nearest',
+            inline: 'nearest',
+            boundary: document.querySelector(".menu-container")
+        });
+    }
+}
+
 const setupGlobal = () => {
 
     // This script should be loaded in a tag with async tag so we can directly apply all the functions
 
     setupNavbarBurger();
+    setupNavbarDropdown();
     setupMobileMenu();
-
-    if (document.querySelector(".menu") !== null) {
-        setupMenuNavigation();
-    }
-
     setupCopyCode();
-
-    // Make the table of content visible
-    const tableOfContentElement = document.querySelector(".table-of-content");
-
-    if (tableOfContentElement !== null) {
-        tableOfContentElement.scrollIntoView({
-            block: "nearest", // Scroll to the nearest parent otherwise it also scroll the main content
-            inline: "nearest" // Scroll to the nearest parent otherwise it also scroll the main content
-        })
-    }
+    scrollMenuOrTableOfContentIfNeeded();
 }
 
-// The page is ready execute our code
+// The page is ready to execute our code
 if (document.readyState === "complete") {
     setupGlobal();
     // The page is not ready, wait for it to be ready
